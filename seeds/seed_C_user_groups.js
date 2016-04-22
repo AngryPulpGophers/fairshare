e1623bb78b6fa4e4b4b29727ead982cbbdca8ce8
Rico Rojas <djizco@yahoo.com>
2016-04-22 11:17:11 -0500

    Update Seed Data

--- seeds/seed_C_user_groups.js
+++ seeds/seed_C_user_groups.js
@@ -1,28 +1,36 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('user_groups').del(), 
+    knex('user_groups').del(),
 
     // Inserts seed entries
     knex('user_groups').insert({
-      user_id: 1, 
+      user_id: 1,
     	group_id: 1
     }),
     knex('user_groups').insert({
-      user_id: 2, 
-    	group_id: 1 
+      user_id: 2,
+    	group_id: 1
     }),
     knex('user_groups').insert({
-      user_id: 2, 
-    	group_id: 3 
+      user_id: 3,
+      group_id: 1
     }),
     knex('user_groups').insert({
-      user_id: 3, 
-    	group_id: 2 
+      user_id: 1,
+    	group_id: 2
     }),
     knex('user_groups').insert({
-      user_id: 1, 
-    	group_id: 3 
+      user_id: 2,
+      group_id: 2
+    }),
+    knex('user_groups').insert({
+      user_id: 1,
+      group_id: 3
+    }),
+    knex('user_groups').insert({
+      user_id: 3,
+    	group_id: 3
     })
   );
 };
--- seeds/seed_D_expenses.js
+++ seeds/seed_D_expenses.js
@@ -1,33 +1,68 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('expenses').del(), 
+    knex('expenses').del(),
 
     // Inserts seed entries
     knex('expenses').insert({
-      title: 'drinks', 
-    	amount : 120.50,
+      title: 'drinks',
+      amount : 120.50,
       group_id: 1,
-      user_id:  1,
+      paid_by:  1,
       img_url: 'http://www.image.com',
       note: 'Happy-hour 12/23'
     }),
-    
+
     knex('expenses').insert({
-      title: 'food', 
-    	amount : 135.50,
-    	group_id: 2,
-    	user_id:  2,
-    	img_url: 'http://www.image.com',
-    	note: 'dinner 12/23'
+      title: 'food',
+      amount : 159.70,
+      group_id: 1,
+      paid_by:  2,
+      img_url: 'http://www.image.com',
+      note: 'Fine Dining 12/23'
     }),
-    
-    knex('expenses').insert({ title: 'food', 
-    	amount : 100.75,
-    	group_id: 3,
-    	user_id:  3,
+
+    knex('expenses').insert({
+      title: 'food',
+      amount : 46.20,
+      group_id: 1,
+      paid_by:  3,
+      img_url: 'http://www.image.com',
+      note: 'Dessert'
+    }),
+
+    knex('expenses').insert({
+      title: 'lunch',
+    	amount : 56.80,
+    	group_id: 2,
+    	paid_by:  1,
     	img_url: 'http://www.image.com',
     	note: 'lunch 12/23'
+    }),
+
+    knex('expenses').insert({
+      title: 'food',
+      amount : 135.50,
+      group_id: 2,
+      paid_by:  2,
+      img_url: 'http://www.image.com',
+      note: 'dinner 12/23'
+    }),
+
+    knex('expenses').insert({ title: 'food',
+    	amount : 100.75,
+    	group_id: 3,
+    	paid_by:  3,
+    	img_url: 'http://www.image.com',
+    	note: 'lunch 12/23'
+    }),
+
+    knex('expenses').insert({ title: 'party',
+      amount : 300.75,
+      group_id: 3,
+      paid_by:  1,
+      img_url: 'http://www.image.com',
+      note: 'Birthday party'
     })
   );
 };
