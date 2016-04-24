1753d43d7ed64935ff1aef3c79054ab0debb5243
Jay McBee <jmcbee1@gmail.com>
2016-04-24 14:45:58 -0500

    minor changes/style

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,23 +1,28 @@
-var passport = require('passport');
+var passport          = require('passport');
 var FacebookStrategy  = require('passport-facebook').Strategy;
-var session = require('express-session');
-var pg = require('pg');
-var pgSession = require('connect-pg-simple')(session);
-var uuid = require('node-uuid');
-var cookieParser = require('cookie-parser');
-var User = require('../models/users');
-var authKeys = require('./auth_secrets.js');
+var session           = require('express-session');
+var pg                = require('pg');
+var pgSession         = require('connect-pg-simple')(session);
+var uuid              = require('node-uuid');
+var cookieParser      = require('cookie-parser');
+var User              = require('../models/users');
+var authKeys          = require('./auth_secrets.js');
 
 
 
 module.exports = function(app,express){
 
-var database_url = process.env.database_url || 'localhost';
+var database_url = process.env.DATABASE_URL || 'localhost';
+
+var trimProfile = function(obj){
+  delete obj.username;
+  delete obj.password;
+  delete obj.facebookId;
+  return obj;
+}
 
 var sessionConfig = {
-  genid: function(){
-    return uuid.v1();
-  },
+  genid: () => uuid.v1(),
   store:  new pgSession({
             pg : pg,                                 
             conString : 'postgresql://' + database_url +'/divvy', 
@@ -34,15 +39,15 @@ app.use(passport.session());
 app.use(cookieParser('kitkat'));
 
 passport.serializeUser(function(user, done) {
+
   return done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
   User.getById({id: id})
-  .then(function(userProfile){
-    delete userProfile.username;
-    delete userProfile.password;
-    done(null, userProfile);
+  .then(function(userObj){
+    var cleanProfile = trimProfile(userObj[0]);
+    done(null, cleanProfile);
   })
   .catch(function(err){
     console.warn("err at deserialize:", err);
@@ -57,15 +62,15 @@ passport.use(new FacebookStrategy(
     profileFields: ['id', 'displayName', 'picture.type(large)','email']
   },
   function(accessToken, refreshToken,params, profile, cb) {
-    console.log('params in fb strat:', params)
-    // console.log('profile:', profile);
+     //console.log('params in fb strat:', params)
 
+    //check DB for user--IF exists, execute cb->line 68
+    //ELSE create profile, store in DB, execute cb->lines 70-85
     User.getByFacebookId(profile.id)
-      .then(function(user){
-        if(user[0]){
-          delete user[0].username;
-          delete user[0].password;
-          return cb(null, user[0]);
+      .then(function(userObj){
+        if(userObj){
+          cleanProfile = trimProfile(userObj[0])
+          return cb(null, cleanProfile);
         }else{
           var userProfile = {
             name: profile.displayName,
@@ -73,15 +78,15 @@ passport.use(new FacebookStrategy(
             password:'',
             email: profile.emails[0].value, 
             facebookId: profile.id,
-            img_url: '' 
+            img_url: profile.photos[0].value
           }
         }
         User.create(userProfile)
         .then(function(id){
+          //attach app ID to userProfile for use in fn serializeUser->line 34
           userProfile.id = id[0];
-          delete userProfile.username;
-          delete userProfile.password;
-          return cb(null, userProfile);
+          cleanProfile = trimProfile(userProfile)
+          return cb(null, cleanProfile);
         })
       })
       .catch(function(err){
@@ -89,6 +94,7 @@ passport.use(new FacebookStrategy(
       })
   }));
 }
+
 // module.exports = function get () {
 //   return [fbProfileInfo, twitProfileInfo];
 // };
