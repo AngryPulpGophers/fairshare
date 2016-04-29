ceb17478bd5edfb26c6d34352b9b286583038db3
lovelydreamer <christinamitchell08@gmail.com>
2016-04-29 12:20:37 -0500

    changed code to be functional w/lodash  DRY style

--- client/components/profile.js
+++ client/components/profile.js
@@ -1,6 +1,63 @@
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
-export const fields = [ 'name', 'username', 'email']
+//export const fields = [ 'name', 'username', 'email'];
+const FIELDS = {
+    name : {
+        type : 'input',
+        label : 'name',
+    },
+    username : {
+        type : 'input',
+        label: 'username'
+    },
+    email : {
+        type : 'input',
+        label: 'email'
+    }
+};
+
+export default class Profile extends Component {
+    onSubmit(props) {
+      alert('Profile Updated');
+    }
+
+    renderField(fieldConfig, field) { // one helper per ea field declared
+      const fieldHelper = this.props.fields[field];
+
+      return (
+         <div>
+          <label>{fieldConfig.label}</label>
+          <div>
+            <fieldConfig.type type="text" value={fieldConfig.label} placeholder={fieldConfig.label} {...fieldHelper}/>
+          </div>
+          {fieldHelper.touched && fieldHelper.error && <div>{fieldHelper.error}</div>}
+        </div>
+      );
+    }
+
+  render() {
+      const {resetForm, handleSubmit, submitting } = this.props;
+
+    return (
+        <div>
+            <img className="image" src={this.props.userInfo.img_url}/>
+            <form onSubmit={handleSubmit}>
+            <div>
+            {_.map(FIELDS, this.renderField.bind(this))}
+              <button type="submit" className="btn btn-primary" disabled={submitting}>
+                {submitting ? <i/> : <i/>} Submit
+              </button>
+              <div> - don't forget to style those buttons</div>
+              <button type="button" className="btn btn-danger" disabled={submitting} onClick={resetForm}>
+                Clear Values
+              </button>
+            </div>
+          </form>
+      </div>
+    );
+  }
+}
 
 // export default class Profile extends Component {
 //     render() {
@@ -17,66 +74,32 @@ export const fields = [ 'name', 'username', 'email']
 
 const validate = values => {
   const errors = {}
- if (!values.name) {
-    errors.name = 'Required'
-  }
-  if (!values.username) {
-    errors.username = 'Required'
-  } else if (values.username.length > 15) {
-    errors.username = 'Must be 15 characters or less'
-  }
-  if (!values.email) {
-    errors.email = 'Required'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
-  }
+     // if (!values.name) {
+     //    errors.name = 'Required'
+     //  }
+     //  if (!values.username) {
+     //    errors.username = 'Required'
+     //  } else if (values.username.length > 15) {
+     //    errors.username = 'Must be 15 characters or less'
+     //  }
+     //  if (!values.email) {
+     //    errors.email = 'Required'
+     //  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+     //    errors.email = 'Invalid email address'
+     //  }
+
+    //type is config obj, field is actual field-name
+
+ _.each(FIELDS, (type, field) => {
+    if (!values[field]) {
+        errors[field] = 'Enter a ${field}'
+    }
+ }); 
+
   return errors
 }
 
-export default class Profile extends Component {
-  render() {
-    const { fields: { username, email, name }, resetForm, handleSubmit, submitting } = this.props
-    return (
-        <div>
-        <img className="image" src={this.props.userInfo.img_url}/>
-        
 
-        <form onSubmit={handleSubmit}>
-        <div>
-        <div>
-          <label>name</label>
-          <div>
-            <input type="text" value="male" placeholder="name" {...name}/>
-          </div>
-          {name.touched && name.error && <div>{name.error}</div>}
-        </div>
-          <label>Username</label>
-          <div>
-            <input type="text" placeholder="Username" {...username}/>
-          </div>
-          {username.touched && username.error && <div>{username.error}</div>}
-        </div>
-        <div>
-          <label>Email</label>
-          <div>
-            <input type="text" placeholder="Email" {...email}/>
-          </div>
-          {email.touched && email.error && <div>{email.error}</div>}
-        </div>
-        <div>
-          <button type="submit" disabled={submitting}>
-            {submitting ? <i/> : <i/>} Submit
-          </button>
-          <div>- don't forget to style those buttons</div>
-          <button type="button" disabled={submitting} onClick={resetForm}>
-            Clear Values
-          </button>
-        </div>
-      </form>
-      </div>
-    )
-  }
-}
 
 // requiring from parent comp.
     // Profile.PropTypes = {
@@ -92,7 +115,7 @@ Profile.propTypes = {
 
 export default reduxForm({
   form: 'Profile',
-  fields,
+  fields: _.keys(FIELDS),
   validate
 })(Profile)
 
