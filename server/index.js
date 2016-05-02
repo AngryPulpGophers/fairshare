a51a3231471e1b16ed21cac1e3823d6634a485f9
Rico Rojas <djizco@yahoo.com>
2016-05-02 13:54:56 -0500

    hotfix again

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV !== 'production') {
   var webpackDevMiddleware = require('webpack-dev-middleware');
   var webpackHotMiddleware = require('webpack-hot-middleware');
   var webpack = require('webpack');
-  var config = require('./webpack.config');
+  var config = require('../webpack.config');
   var compiler = webpack(config);
 
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
