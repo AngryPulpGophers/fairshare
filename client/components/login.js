61a79f0978735329121141502a6c607ea074e98c
Jay McBee <jmcbee1@gmail.com>
2016-05-04 22:14:06 -0500

    remove hacky functions

--- client/components/login.js
+++ client/components/login.js
@@ -2,11 +2,6 @@ import React, { Component, PropTypes } from 'react';
 
 export default class Login extends Component {
   
-  setSessionStorage (thirdParty) {
-    sessionStorage.setItem(thirdParty, true);
-    sessionStorage.setItem('modal',true);
-  }
-
 
   render() {
     return (
@@ -17,13 +12,13 @@ export default class Login extends Component {
               <h3>Want to track your expenses? You gotta' login!</h3>
               <div className="row">
                 <div className="small-12 large-4 columns">
-                  <a href= 'auth/facebook' onClick ={()=> this.setSessionStorage('facebook')} className="large primary button"><i className="fa fa-facebook-square"></i> Facebook</a>
+                  <a href= 'auth/facebook' className="large primary button"><i className="fa fa-facebook-square"></i> Facebook</a>
                 </div>
                 <div className="small-12 large-4 columns">
-                  <a href='/auth/google' onClick ={()=> this.setSessionStorage('google')} className="large alert button"><i className="fa fa-google-plus-square"></i> Google +</a>
+                  <a href='/auth/google' className="large alert button"><i className="fa fa-google-plus-square"></i> Google +</a>
                 </div>
                 <div className="small-12 large-4 columns">
-                  <a href='/auth/paypal' onClick ={()=> this.setSessionStorage('paypal')} className="large secondary button"><i className='fa fa-paypal'></i> PayPal</a>
+                  <a href='/auth/paypal' className="large secondary button"><i className='fa fa-paypal'></i> PayPal</a>
                 </div>
               </div>
             </div>
