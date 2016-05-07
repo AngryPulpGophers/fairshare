11b343151cbb1e9390ef47fd318cde7259963485
Rico Rojas <djizco@yahoo.com>
2016-05-07 17:30:03 -0500

    Update dashboard.js

--- server/models/dashboard.js
+++ server/models/dashboard.js
@@ -13,9 +13,3 @@ Dashboard.getUserOwes = function(id) {
 Dashboard.payments = function(id) {
   return db.raw("SELECT (SELECT SUM(amount) as got FROM payments WHERE recipient = " + id + ") - (SELECT SUM(amount) as gave FROM payments WHERE payee = " + id + ") as total;");
 }
-
-
-
-
-
-// "SELECT SUM(owe) FROM (SELECT expenses.title, expenses.amount, (expenses.amount/count(*)) AS owe FROM expenses LEFT JOIN user_expenses ON (expenses.id = user_expenses.expense_id) WHERE expenses.paid_by != "+id+" and expense_id in (select expense_id from users LEFT JOIN user_expenses ON ("+id+" = user_expenses.user_id)) GROUP BY expenses.id) AS total;");
