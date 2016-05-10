64ccfcb469a6a32de2b55ae7a315f637a9392db9
pmatteu2 <pmatteu2@gmail.com>
2016-05-10 12:44:36 -0500

    made progress but showing same info on each form

--- client/components/groupView.js
+++ client/components/groupView.js
@@ -6,7 +6,8 @@ import {prettyDate, calcBalance, makeGroupObj, test} from '../utility/groupViewH
 import Modal from './modal';
 import PaymentForm from './paymentForm';
 import AddExpense from './addExpense';
-import defaultPicture from '../images/defaultPicture.jpg'
+import defaultPicture from '../images/defaultPicture.jpg';
+import UpdateExpense from './updateExpense';
 
 export default class GroupView extends Component {
   constructor(props){
@@ -35,13 +36,13 @@ export default class GroupView extends Component {
     //console.log('pjpjpjp',currentURL.split('/')[2])
   var ID = currentURL.split('id=')
   // console.log(this.props.activity)
-  //console.log('maybe work222', this.props.currentGroupUsers)
+  console.log('maybe work222', this.props.currentGroupUsers)
   //console.log('groupView CurrentUser',this.props.userInfo)
   var showUserBalance=[];
   showUserBalance=calcBalance.call(this)//this.calcBalance();
   //console.log('hi pj, stuff should be here^^^^', showUserBalance)
   var localGroupObj=makeGroupObj.call(this) //this.makeGroupObj()
-  
+  var expenseValues = {};
 
 
   // setting this to bypas the need for authentication
@@ -102,12 +103,39 @@ export default class GroupView extends Component {
       {/*<p> hi add expense<Link  to={{pathname:'/addExpense',query:{ id:ID[1] , pj:'holly'}}} title="groupView"  className="button primary float-left tiny button">Add Expense</Link> </p>*/}
 
          {this.props.activity.map(function(activity,index){
+          var holdMemberId=[]
+          expenseValues[activity.id]= {
+            title: activity.title,
+            note: activity.note,
+            amount: Number(activity.amount),
+          }
+          for(var x =0 ; x< activity.members.length ; x++){
+            holdMemberId.push(activity.members[x].id);
+          }
+          for (var i = 0; i < this.props.currentGroupUsers.length ; i++){
+            if (holdMemberId.indexOf(this.props.currentGroupUsers[i].user_id) !== -1){
+              expenseValues[activity.id]['members'+i] = true;
+            }
+          }
+          
+          console.log('all my hard work for this',expenseValues[activity.id])
             return <div key={activity.id + activity.type} className= {activity.type==='expense' ? "callout alert" :"callout success"}>
               
               {activity.type==='expense' ?
               <div>
                 <div>Title: {activity.title} Time:{prettyDate(activity.created_at)} Amount: ${activity.amount}    
                 <button title="groupView"  className="button primary tiny button" onClick={()=>this.props.toggleDisplay(index)}>details</button>
+                  <UpdateExpense
+                  {...{initialValues:expenseValues[activity.id]}}
+                    getActivity={this.props.getActivity}
+                    activity={this.props.activity}
+                    currentGroupUsers = {this.props.currentGroupUsers}
+                    url = {this.props.url}
+                    getUserByGroup = {this.props.getUserByGroup}
+                    addExpense = {this.props.addExpense}
+                    userInfo = {this.props.userInfo}
+                    activityID = {activity.id}
+                  />
                   <div style={this.props.displayActive[index]}>
                     <div className = 'row '>
                       <div className = 'small-12 large-6 columns'>
---
+++ client/components/updateExpense.js
@@ -0,0 +1,230 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
+import { reduxForm } from 'redux-form';
+import PureInput from './PureInput';
+import Modal from './modal';
+import request from 'superagent';
+export const fields = [ 'title', 'note', 'imgUrl', 'photo', 'members0','members1','members2',
+  'members3', 'members4','members5','members6','members7','members8','members9', 'amount'];
+
+export default class UpdateExpense extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = ({isModalOpen: false});
+    //this.state = ({initialValue: true})
+  }
+  openModal = () => {
+    this.setState({isModalOpen: true})
+  }
+  closeModal = () => {
+    this.setState({isModalOpen: false})
+  }
+
+  componentWillMount(){
+    console.log('currentURL type',typeof window.location.href )
+    //call our get groups function only if we haven't called it yet
+    var currentURL = window.location.href;
+    console.log(currentURL);
+    var ID = currentURL.split('id=');
+    //ID= ID[1].split('&')
+    this.props.getUserByGroup(ID[1]);
+    console.log('maybe work33', this.props.currentGroupUsers);
+    console.log(this,'this');
+  }
+
+
+  handleSubmit(data) {
+    if (this.props.userInfo){
+    var photo = new FormData();
+    console.log('PHOTO',data.photo)
+    if(data.photo == undefined){
+      data.photo = [];
+    }
+    if (data.photo.length){
+      photo.append('photo', data.photo[0]);
+    
+
+      request.post('/groups/expenses/upload')
+      .send(photo)
+      .end(function(err, resp) {
+        if (err) { console.error(err); }
+        return resp;
+      })
+      .then(function(resp){
+        var currentURL = window.location.href;
+        var baseURL = currentURL.split('/')[0];
+            console.log(currentURL);
+        var ID = currentURL.split('id=');
+        //ID= ID[1].split('&')
+        var obj = {}
+        obj.members = []
+        //hard coded to 10, which is number of hard coded members max
+        for (var i = 0 ; i< this.props.currentGroupUsers.length ; i++){
+
+            if (this.props.userInfo.id===this.props.currentGroupUsers[i].user_id){
+              data['members'+i] = true;
+            }
+
+        }
+        for (var i = 0 ; i< 10 ; i++){
+          if (data['members'+i]){
+            obj.members.push(this.props.currentGroupUsers[i].user_id)
+          }
+        }
+        obj.paid_by = this.props.userInfo.id;
+        obj.title = data.title;
+        obj.amount = Number(Number(data.amount).toFixed(2))
+        obj.img_url = resp.text //|| data.imgUrl;
+        obj.note = data.note;
+        obj.group_id = Number(ID[1]);
+        console.log('send to post expense', obj);
+        this.setState({isModalOpen:false})
+        this.props.destroyForm();
+        this.props.addExpense(JSON.stringify(obj));
+
+        //location.replace(baseURL+'/groupView?id='+ID[1]);
+      }.bind(this));
+    }
+    else{
+      
+      var currentURL = window.location.href;
+        var baseURL = currentURL.split('/')[0];
+            console.log(currentURL);
+        var ID = currentURL.split('id=');
+        //ID= ID[1].split('&')
+        var obj = {}
+        obj.members = []
+        //hard coded to 10, which is number of hard coded members max
+        for (var i = 0 ; i< this.props.currentGroupUsers.length ; i++){
+
+            if (this.props.userInfo.id===this.props.currentGroupUsers[i].user_id){
+              data['members'+i] = true;
+            }
+
+        }
+        for (var i = 0 ; i< 10 ; i++){
+          if (data['members'+i]){
+            obj.members.push(this.props.currentGroupUsers[i].user_id)
+          }
+        }
+        obj.paid_by = this.props.userInfo.id;
+        obj.title = data.title;
+        obj.amount = Number(Number(data.amount).toFixed(2))
+        obj.img_url = 'client/images/download.jpg'
+        obj.note = data.note;
+        obj.group_id = Number(ID[1]);
+        console.log('send to post expense', obj);
+        this.setState({isModalOpen:false})
+        this.props.destroyForm();
+        this.props.addExpense(JSON.stringify(obj));
+
+        //location.replace(baseURL+'/groupView?id='+ID[1]);
+    }
+    }
+    else{
+      console.log("ERROR NO CURRENT USER");
+    }
+}
+
+
+  render(){
+  
+
+    
+
+    const {
+
+      fields: { title, note, imgUrl, photo, members0,members1,members2,
+                members3, members4, members5, members6, members7, members8, members9, amount },
+      handleSubmit,
+      resetForm,
+      submitting
+      } = this.props
+
+
+    return (
+      <div>
+      <button className = 'button primary button tiny'onClick={this.openModal}>Edit Expense</button>
+            <Modal isOpen={this.state.isModalOpen}
+                   transitionName="modal-anim">
+      <form onSubmit={this.props.handleSubmit(this.handleSubmit.bind(this)).bind(this)}>
+        <i onClick={this.closeModal} className="fa fa-times-circle-o" aria-hidden="true" style = {{cursor:'pointer'}}></i>
+        <div>
+          <h2>Add Expense</h2>
+          <label>Title</label>
+          <div>
+            <input type="text" placeholder="Title" required{...title}/>
+          </div>
+        </div>
+        <div>
+          <label>Note</label>
+          <div>
+            <input type="text" placeholder="Note" required{...note}/>
+          </div>
+        </div>
+        {/*
+        <div>
+          <label>imgUrl</label>
+          <div>
+            <input type="text" placeholder="Image URL" {...imgUrl} />
+          </div>
+        </div>
+      */}
+        <div>
+          <label>Upload a photo (overwrites url option)</label>
+          <div>
+            <input type="file" accept='image/*' {...photo} value={null} />
+          </div>
+        </div>
+
+        <div>
+          <label>Amount</label>
+          <div>
+            <input type="text" placeholder="Amount(must be of form ##.##)" pattern = '[0-9]{1,}\.[0-9]{2}' title = 'Must be in format ##.##' required{...amount}/>
+          </div>
+        </div>
+        <div>
+        <h3>Members Involved</h3>
+        {this.props.currentGroupUsers.map(function(user,index){
+          //console.log(members[index],'what is this')
+          // console.log('big thing',this)
+       //members.push({value:true})
+         var string = 'members'+index
+         if(this.props.userInfo.id===user.user_id){
+         // console.log('pj',true)
+       }
+          return (
+          <label>
+              {this.props.userInfo.id===user.user_id ?
+               <PureInput type="checkbox"   checked='checked' field = {this.props.fields[string]}/>
+
+               :
+               <PureInput type="checkbox"  field = {this.props.fields[string]}/>
+
+             } <span>{user.name}</span>
+          </label>)
+          }.bind(this))
+            }
+        </div>
+
+        <div>
+          <button type="submit" className="button primary float-left tiny button" disabled={submitting}>
+            {submitting ? <i/> : <i/>} Update
+          </button>
+          <button type="button" disabled={submitting} onClick={resetForm}>
+            Clear Values
+          </button>
+        </div>
+      </form>
+      </Modal>
+      </div>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'complex'+this.props.activityID,
+  fields
+})(UpdateExpense)
