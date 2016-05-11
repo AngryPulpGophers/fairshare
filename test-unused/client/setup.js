5552cbd01424a1a8440689532c7347a685d125ff
Rico Rojas <djizco@yahoo.com>
2016-05-10 19:28:45 -0500

    moved test file

---
+++ test-unused/client/setup.js
@@ -0,0 +1,3 @@
+"use strict";
+var chai = require("chai");
+global.expect = chai.expect;
--- test/client/setup.js
+++
@@ -1,5 +0,0 @@
-// this is to save time writing these imports, please don't delete (provides chai globally)
-
-"use strict";
-var chai = require("chai");
-global.expect = chai.expect;
