465b6339824a4af00be3010568a37fa965486ef7
Sam Fitz <abigwonderful@gmail.com>
2016-05-12 14:58:11 -0500

    setting enviro to prod

--- client/containers/root.js
+++ client/containers/root.js
@@ -1,8 +1,8 @@
 //delete ||true on next line to set to development
 if (process.env.NODE_ENV === 'production' || true) {
-  console.log('our enviro:', process.env.NODE_ENV)
+  console.log('our production enviro')
   module.exports = require('./root.prod');
 } else {
-  console.log('our enviro:', process.env.NODE_ENV)
+  console.log('our development enviro')
   module.exports = require('./root.dev');
 }
--- client/store/configure-store.js
+++ client/store/configure-store.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || true) {
+  console.log('our production enviro')
   module.exports = require('./configure-store.prod');
 } else {
+  console.log('our testing enviro')
   module.exports = require('./configure-store.dev');
 }
