a08a54a2b2f4d82148f1edb84a823d7b5a5f678b
lovelydreamer <christinamitchell08@gmail.com>
2016-05-13 11:44:11 -0500

    rebase

--- test-client/specs/cmpnt-profile.spec.js
+++ test-client/specs/cmpnt-profile.spec.js
@@ -5,6 +5,8 @@ import Profile from "../../client/components/profile";
 describe('Front End @Profile Component', () => {
   const wrapper = shallow(<Profile/>);
 
+  console.log("wrapper_renderedComponent inside of profile ", wrapper.props);
+
     it ('should contain three form input fields ', () => {
       expect(wrapper.prop('fields').length).to.eql(3);
     });
@@ -26,6 +28,7 @@ describe('Front End @Profile Component', () => {
     it('has a form ', () => {
         wrapper.find('div').forEach(function (node) {
         expect(node.hasClass('.profile-form')).to.equal(true);
+
       });
     });
 
