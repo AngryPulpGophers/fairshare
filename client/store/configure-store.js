d79ee45d46614b6afd7f6dc46b32f28ef6ae1c91
Rico Rojas <djizco@yahoo.com>
2016-05-13 19:30:09 -0500

    bringing back dev-tools

--- client/store/configure-store.js
+++ client/store/configure-store.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === 'production' || true) {
-  console.log('our production enviro')
+if (process.env.NODE_ENV === 'production' || false) {
+  console.log('our production enviro');
   module.exports = require('./configure-store.prod');
 } else {
-  console.log('our testing enviro')
+  console.log('our testing enviro');
   module.exports = require('./configure-store.dev');
 }
